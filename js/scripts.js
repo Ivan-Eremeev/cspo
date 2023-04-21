b5e937c4602af9a9438e51e97bf801385089b2ae
Ivan <ivan.frontcoder@gmail.com>
2023-04-21 17:11:10 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,53 @@ window.onload = function () {
   };
   stikyMenu($('#headerSticky'));
 
+  // Выпадайки при клике по кнопке
+  // Задать блокам выпадайкам айдишник совпадающий с data-drop="" в кнопке для этого блока
+  // Задать кнопкам .js-drop-btn и data-drop="" с айдишником блока выпадайки
+  function dropBlock(btn, lock = false) {
+    let $this = undefined,
+      drop = undefined,
+      close = $('.js-drop-close'),
+      body = $('body');
+    btn.on('click', function () {
+      let $this = $(this);
+      let drop = $('#' + $this.data('drop'));
+      let scrollWidth = (window.innerWidth - $(window).width());
+      if (!$this.hasClass('is-active')) {
+        $this.addClass('is-active');
+        drop.addClass('open');
+        if (lock) {
+          body.toggleClass('lock');
+          body.css('padding-right', scrollWidth);
+        }
+      } else {
+        $this.removeClass('is-active');
+        drop.removeClass('open');
+        body.removeClass('lock');
+        body.css('padding-right', 0);
+      }
+      $(document).mouseup(function (e) {
+        if (!$this.is(e.target)
+          && $this.has(e.target).length === 0
+          && !drop.is(e.target)
+          && drop.has(e.target).length === 0) {
+          $this.removeClass('is-active');
+          drop.removeClass('open');
+          body.removeClass('lock');
+          body.css('padding-right', 0);
+        }
+      });
+    })
+    close.on('click', function () {
+      $('[data-drop="' + $(this).data('drop') + '"]').removeClass('is-active');
+      $('#' + $(this).data('drop')).removeClass('open');
+      body.removeClass('lock');
+      body.css('padding-right', 0);
+    })
+  }
+  dropBlock($('.js-drop-btn'));
+  dropBlock($('.js-drop-menu'), true);
+
   // // Swiper | Слайдер
   // if ($('#swiper').length) {
   //   const swiper = new Swiper('#swiper', {
@@ -94,6 +141,41 @@ window.onload = function () {
       sliderToggle();
     });
   }
+
+  // Анимация счетчика
+	function countNumber(block) {
+		block.each(function () {
+			var scrollTop = false,
+				countNumberStatus = true,
+				$this = $(this),
+				blockPosition = $this.position().top,
+				valUp = $this.data('val-up'),
+				valTo = $this.data('val-to'),
+				valDuration = $this.data('duration'),
+        valDelay = $this.data('delay');
+			$this.html(0);
+      gofunc();
+			$(window).scroll(function () {
+        gofunc();
+			});
+      function gofunc() {
+        scrollTop = $(window).scrollTop() + $(window).height() - 150;
+        if (scrollTop > blockPosition && countNumberStatus) {
+          setTimeout(() => {
+            $({ numberValue: valUp }).animate({ numberValue: valTo }, {
+              duration: valDuration,
+              easing: "swing",
+              step: function (val) {
+                $this.html(Math.ceil(val));
+              }
+            });
+          }, valDelay);
+          countNumberStatus = false;
+        }
+      }
+		});
+	};
+	countNumber($(".count-number"));
 
   // // Air Datepicker | Календарь
   // new AirDatepicker('#airDatepicker', {
@@ -305,53 +387,6 @@ window.onload = function () {
   //     rangeSlider.noUiSlider.set([null, this.value]);
   //   });
   // };
-
-  // Выпадайки при клике по кнопке
-  // Задать блокам выпадайкам айдишник совпадающий с data-drop="" в кнопке для этого блока
-  // Задать кнопкам .js-drop-btn и data-drop="" с айдишником блока выпадайки
-  function dropBlock(btn, lock = false) {
-    let $this = undefined,
-        drop = undefined,
-        close = $('.js-drop-close'),
-        body = $('body');
-    btn.on('click', function () {
-      let $this = $(this);
-      let drop = $('#' + $this.data('drop'));
-      let scrollWidth = (window.innerWidth - $(window).width());
-      if (!$this.hasClass('is-active')) {
-        $this.addClass('is-active');
-        drop.addClass('open');
-        if (lock) {
-          body.toggleClass('lock');
-          body.css('padding-right', scrollWidth);
-        }
-      } else {
-        $this.removeClass('is-active');
-        drop.removeClass('open');
-        body.removeClass('lock');
-        body.css('padding-right', 0);
-      }
-      $(document).mouseup(function (e) {
-        if (!$this.is(e.target)
-          && $this.has(e.target).length === 0
-          && !drop.is(e.target)
-          && drop.has(e.target).length === 0) {
-          $this.removeClass('is-active');
-          drop.removeClass('open');
-          body.removeClass('lock');
-          body.css('padding-right', 0);
-        }
-      });
-    })
-    close.on('click', function () {
-      $('[data-drop="' + $(this).data('drop') +'"]').removeClass('is-active');
-      $('#' + $(this).data('drop')).removeClass('open');
-      body.removeClass('lock');
-      body.css('padding-right', 0);
-    })
-  }
-  dropBlock($('.js-drop-btn'));
-  dropBlock($('.js-drop-menu'), true);
 
   // AOS || Анимация появления блоков
   AOS.init({
